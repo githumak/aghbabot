670b7a444d0b23065a1de7a5f5017d5f5493159a
githumak <30843469+githumak@users.noreply.github.com>
2017-08-09 04:26:25 +0430

    Update easy.php

--- easy.php
+++ easy.php
@@ -115,7 +115,7 @@ function processMessage($message) {
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
   }
 }
-define('WEBHOOK_URL', 'https://github.com/githumak/aghbabot/new/master');
+define('WEBHOOK_URL', 'https://github.com/githumak/aghbabot/new/master/easy.php/');
 if (php_sapi_name() == 'cli') {
   // if run from console, set or delete webhook
   apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
